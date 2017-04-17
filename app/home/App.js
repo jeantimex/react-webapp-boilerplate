ca9271f6b5e702ebab3cad81b5c664e221400804
Yong Su <ysu@mbp-003384.ad.whirl.net>
2017-04-16 21:56:46 -0700

    Scaffold with basic React and Webapp.

---
+++ app/home/App.js
@@ -0,0 +1,18 @@
+import React, { Component, PropTypes } from 'react';
+
+import './style.css';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="viewport">
+        Hello World!
+      </div>
+    );
+  }
+
+}
+
+export default App;
+
---
+++ app/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+import App from 'home/App';
+
+import 'index.html';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
